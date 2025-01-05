1cf987964476787410c0fe127cc677c39e920b23
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-01-05 09:41:56 -0300

    chore: adding rooms table and updating all tables informations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,20 @@
-import { boolean, integer, pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+import { integer, pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
 
 export const playersTable = pgTable('players', {
-	id: uuid().primaryKey().unique().notNull(),
-	name: varchar().notNull(),
-	amount: integer().notNull(),
-	banker: boolean().notNull(),
+	id: uuid().primaryKey().unique().defaultRandom(),
+	name: varchar().notNull().unique(),
+})
+
+export const roomsTable = pgTable('rooms', {
+	id: uuid().primaryKey().unique().defaultRandom(),
+	players: uuid().array(),
+	banker: uuid().notNull(),
 })
 
 export const transfersTable = pgTable('transfers', {
-	id: uuid().primaryKey().unique().notNull(),
-	sender: uuid().notNull(),
-	receiver: uuid().notNull(),
+	id: uuid().primaryKey().unique().defaultRandom(),
+	from: uuid().notNull(),
+	to: uuid().notNull(),
 	amount: integer().notNull(),
+	room: uuid().notNull().unique(),
 })
