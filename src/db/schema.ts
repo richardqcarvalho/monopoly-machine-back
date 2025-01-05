111063b6faae1582ba25fbebf50e4b8923c4dc1c
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-01-05 10:03:58 -0300

    chore: adding table references

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,14 +7,26 @@ export const playersTable = pgTable('players', {
 
 export const roomsTable = pgTable('rooms', {
 	id: uuid().primaryKey().unique().defaultRandom(),
-	players: uuid().array(),
-	banker: uuid().notNull(),
+	players: uuid()
+		.array()
+		.notNull()
+		.references(() => playersTable.id),
+	banker: uuid()
+		.notNull()
+		.references(() => playersTable.id),
 })
 
 export const transfersTable = pgTable('transfers', {
 	id: uuid().primaryKey().unique().defaultRandom(),
-	from: uuid().notNull(),
-	to: uuid().notNull(),
+	from: uuid()
+		.notNull()
+		.references(() => playersTable.id),
+	to: uuid()
+		.notNull()
+		.references(() => playersTable.id),
 	amount: integer().notNull(),
-	room: uuid().notNull().unique(),
+	room: uuid()
+		.notNull()
+		.unique()
+		.references(() => roomsTable.id),
 })
