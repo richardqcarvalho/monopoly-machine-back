984a0644952b3c7f2a45e0b62462c125f03e7937
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-18 10:34:51 -0300

    chore: updating drizzle to new db structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   out: './src/db/migrations',
-  schema: './src/db/schema.ts',
+  schema: [
+    './src/db/schemas/player.ts',
+    './src/db/schemas/room.ts',
+    './src/db/schemas/transfer.ts',
+  ],
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
